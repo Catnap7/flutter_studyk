f50947c29731269c27610c4ca6ddc9ab70b9d372
JaeWooJang <jjw33063@gmail.com>
2023-04-18 00:44:11 +0900

    add Hero
    infodetailscreen에 hero 위젯 추가

--- lib/info/view/info_detail_screen.dart
+++ lib/info/view/info_detail_screen.dart
@@ -12,16 +12,47 @@ class InfoDetailScreen extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-
     final state = ref.watch(membersDetailProvider(name));
 
     Logger().i(state.toList());
 
     return DefaultLayout(
-      child: Center(
-        child: Text(state.map((e) => e.koreanName).toString()),
-      ),
+      child: _memberCard(state),
       title: '정보 상세',
+    );
+  }
+
+  _memberCard(state) {
+    final heroKey = state.map((e) => e.heroKey)?.toString();
+    final image = state.map((e) => e.image)?.toString().toLowerCase();
+
+    return Column(
+      children: [
+        if (heroKey != null)
+          Hero(
+            tag: ObjectKey(heroKey),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(12.0),
+              child: // Asset image
+                  Image.asset(
+                'asset/img/$image/${image}_1.png',
+                // AssetImage('asset/img/$e/${image}_1.png'),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+        if (heroKey == null)
+          ClipRRect(
+            borderRadius: BorderRadius.circular(12.0),
+            child: // Asset image
+                Image.asset(
+              'asset/img/$image/${image}_1.png',
+              // AssetImage('asset/img/$e/${image}_1.png'),
+              fit: BoxFit.cover,
+            ),
+          ),
+        Text(state.map((e) => e.koreanName).toString()),
+      ],
     );
   }
 }
